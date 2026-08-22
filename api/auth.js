db490c6feaf6dc5f03aeadd290377b387189392e
dredscruz <dredscruz@users.noreply.github.com>
2026-08-23 00:51:10 +0400

    Allow GET for auth me action

--- api/auth.js
+++ api/auth.js
@@ -2,7 +2,6 @@ import crypto from 'crypto';
 import { getPool, ensureSchema } from '../lib/db.js';
 
 export default async function handler(req, res) {
-  if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
   await ensureSchema();
   const { action } = req.query;
   const b = req.body || {};
